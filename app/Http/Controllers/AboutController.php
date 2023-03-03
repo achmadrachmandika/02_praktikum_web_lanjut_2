d758f4c573a50866aab5bb2e6eed6b556a7156df
achmadrachmandika <fourhope666@gmail.com>
2023-03-03 13:22:42 +0700

    kikipraktikum2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return "Achmad Rachmandika <br> 2041720125";
+    }
+}
